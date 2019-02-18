772dbaf68a45495ec95f5bdab90b7ddd399fb927
Panchalingamoorthy Gajenthran <p.gajenthran@gmail.com>
2019-02-18 01:19:00 +0100

    hog feature + db image

--- faceDetection.m
+++ faceDetection.m
@@ -1,22 +1,33 @@
-ObjectDetector = vision.CascadeObjectDetector;
-% shapeInserter = vision.ShapeInserter('BorderColor','Custom','CustomBorderColor',[0 255 255]);
+%% Charger les images sources du dossier 'img_src'
+srcImg = imageSet('img_src','recursive');
 
-I = imread('img/hazard.jpg');
+%% Afficher l'ensemble des images sources
+figure;
+montage(srcImg(1).ImageLocation);
+title('Images of Single Face');
+
+%% Charger une image en entrée et reconnaître seulement le visage
+ObjectDetector = vision.CascadeObjectDetector;
+inputImg = read(srcImg(1),1);
 
 % Transformation de l'image en noir et blanc
-if(size(I, 3) > 1)
-    I = rgb2gray(I);
-end;
+if(size(inputImg, 3) > 1)
+    inputImg = rgb2gray(inputImg);
+end
 
 % Detection des visages
-IObject = step(ObjectDetector, I);
-% I = insertShape(I, 'Rectangle', IObject);
-% bbox = step(shapeInserter, I, int32(IObject));
-IFaces = imcrop(I, IObject(1, :));
-imwrite(IFaces, 'face.jpg');
+inObject = step(ObjectDetector, inputImg);
+inputImg = imcrop(inputImg, inObject(1, :));
+% imwrite(inFace, 'face.jpg');
+imshow(inputImg), title('Face cropping');
 
-% Affichage de l'image
-imshow(I), title('Face recognition');
 
-imshow(img);
+%% Extraction de caracteristiques avec HoG (Histogram of Gradient)
+[hogFeature, hogImg] = ...
+    extractHOGFeatures(inputImg);
+figure;
+subplot(1,2,1);imshow(inputImg);title('Input image');
+subplot(1,2,2);plot(hogImg);title('HoG image');
+
+
 
